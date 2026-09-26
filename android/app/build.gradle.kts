1c6023613635e99872928f11f623359eb997bd9d
marekfx77-netizen <marek.fx77@gmail.com>
2026-09-26 14:58:37 +0200

    chore: Update applicationId to com.mermiko.sshmobile for MerMiko brand

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.marekfx77.sshmobile"
+        applicationId = "com.mermiko.sshmobile"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
