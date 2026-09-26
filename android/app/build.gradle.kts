0964d26b020698b48126670d71d4ed56b16bbad3
marekfx77-netizen <marek.fx77@gmail.com>
2026-09-26 17:43:37 +0200

    feat: bump targetSdk to 36 (Android 16) and versionCode to 2 for Google Play

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,14 +5,14 @@ plugins {
 
 android {
     namespace = "com.marekfx77.sshmobile"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.mermiko.sshmobile"
         minSdk = 26
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        targetSdk = 36
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
